f80f261de5eb21795b00f0a3de8b81dd1b4d5fbf
Abhishek <abhishekyadav1999.com@gmail.com>
2024-08-24 15:25:36 +0530

    bgChangerCode

--- bgChanger/src/App.jsx
+++ bgChanger/src/App.jsx
@@ -1,13 +1,23 @@
 import { useState } from 'react'
 
-import './App.css'
 
 function App() {
-  
-
+  const [color,setColor] = useState("#64748b")
   return (
     <>
-    <div className="text-3xl">Abhishek</div>
+    <div className="h-screen w-full duration-200 text-white"
+    style= {{backgroundColor:color}}> 
+    <div className="fixed flex  justify-center  bottom-12 inset-x-0">
+      <div className="flex p-4 bottom-12  gap-4 bg-white rounded-3xl shadow-lg">
+        <button onClick={()=>setColor("green")} className="bg-green-700 px-3 py-1 rounded-3xl shadow-lg">Green</button>
+        <button onClick={()=>setColor("blue")} className="bg-blue-900 px-3 py-1 rounded-3xl shadow-lg">Blue</button>
+        <button onClick={()=>setColor("black")} className="bg-black px-3 py-1 rounded-3xl shadow-lg">Black</button>
+        <button onClick={()=>setColor("white")} className="bg-white text-black px-3 py-1 rounded-3xl shadow-lg">White</button>
+        <button onClick={()=>setColor("#334155")} className="bg-slate-700 px-3 py-1 rounded-3xl shadow-lg">Slate</button> 
+      </div>
+    </div>
+    </div>
+    
     </>
   )
 }
