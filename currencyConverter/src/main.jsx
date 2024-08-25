14302ff287ef5fe0f1122dbfe9daf14ff9c599ae
Abhishek <abhishekyadav1999.com@gmail.com>
2024-08-25 16:14:42 +0530

    making Hook and Compo

--- currencyConverter/src/main.jsx
+++ currencyConverter/src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import InputBox from './Components/InputBox.jsx'
+import useCurrencyInfo from './Hooks/UseCurrencyInfo.js'
+// import App from './App.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <InputBox/>,
+    <useCurrencyInfo/>
   </StrictMode>,
 )
